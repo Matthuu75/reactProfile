042a6bc1e7b54d0993b5494921f305e2c5624419
Marcus Matthews <mlmatthews75@gmail.com>
2023-12-07 16:41:44 -0500

    fixing errors to deploy to netlify site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,14 @@ export const Footer = () => {
       <footer id="footer">
         <a href="https://github.com/Matthuu75">
           <img
-            src="./src/assets/GitHub-logo.png"
+            src="https://drive.google.com/file/d/15DZQSza3MnbzTYIaQr_PRmVt6HY_-VPc/view?usp=drive_link"
             className="githubLogo"
             alt=""
           />
         </a>
         <a href="https://www.linkedin.com/in/marcus-matthews-2655a5119/">
           <img
-            src="./src/assets/linkedin.png"
+            src="https://drive.google.com/file/d/1WWrODSnvNTu9ASoWBo1NPM5cZjuUfhxw/view?usp=drive_link"
             className="linkedInLogo"
             alt=""
           />
